2258b2490c3ddf8ef4e3fbedf1cf32d1a6e420f2
Leonardo Cezar Russi Cunha <leocunha20030@hotmail.com>
2024-11-27 22:40:30 -0300

    CursorLockState menus

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -8,6 +8,10 @@ public class MenuManager : MonoBehaviour
 
     void Start()
     {
+        // Ativa o cursor ao carregar a cena do menu
+        Cursor.lockState = CursorLockMode.None; // Libera o cursor
+        Cursor.visible = true;                 // Torna o cursor visível
+
         // Exibe os dados armazenados em GameData
         jumpCountText.text = "Pulos: " + GameData.jumpCount;
         timerText.text = "Tempo: " + GameData.totalTime.ToString("F2") + " segundos";
